c67f44f3322f8c4130d9b6c720eb192057708c5b
jeeseongmin <peration0422@exntu.com>
2024-04-16 10:46:42 +0900

    feat : 로그인 페이지 레이아웃 개발

---
+++ src/pages/Login/components/LoginLayout.tsx
@@ -0,0 +1,38 @@
+import { PropsWithChildren } from "react";
+
+import login from "@/assets/images/login.png";
+
+const LoginLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <section className="flex h-2/3 w-2/3 flex-col rounded-md border border-gray-300 shadow-lg xl:flex-row">
+        <div className="relative h-1/2 w-full xl:h-full xl:w-1/2">
+          <img
+            src={login}
+            className="h-full w-full rounded-l-md object-cover"
+          />
+          <div className="absolute right-0 top-0 h-full w-full rounded-l-md bg-blue-900/30 px-12 py-8 text-white xl:py-20">
+            <p className="text-md font-bold">Shalom!</p>
+            <p className="text-xl font-bold">평택 온누리교회 장소 예약</p>
+            <div className="absolute left-0 top-0 hidden h-full w-full flex-col items-center justify-end px-12 pb-16 text-center md:flex xl:justify-center">
+              <div className="flex flex-col items-center justify-center">
+                <p className="text-sm">
+                  “하나님의 나라를 전파하며 주 예수 그리스도에 관한 모든 것을
+                  담대하게 거침없이 가르치더라”
+                </p>
+                <br />
+                <p className="font-bold">사도행전 28:31</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex h-full w-full flex-col items-center justify-center px-12 xl:w-1/2">
+          {children}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LoginLayout;
