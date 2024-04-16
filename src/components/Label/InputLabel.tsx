9b678bc61f84867090af90b7a6ffe08fbd36dc44
jeeseongmin <peration0422@exntu.com>
2024-04-16 22:58:51 +0900

    feat : input label 컴포넌트 개발

---
+++ src/components/Label/InputLabel.tsx
@@ -0,0 +1,16 @@
+import { ComponentPropsWithoutRef } from "react";
+
+interface InputLabelProps extends ComponentPropsWithoutRef<"label"> {
+  text: string;
+  isRequired: boolean;
+}
+
+const InputLabel = ({ text, isRequired, ...props }: InputLabelProps) => {
+  return (
+    <label className="text-md" {...props}>
+      {text}
+      {isRequired && <span className="ml-1 text-red-500">*</span>}
+    </label>
+  );
+};
+export default InputLabel;
