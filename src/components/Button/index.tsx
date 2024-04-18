563111fe918eb4070872da98e643caffffc0192a
jeeseongmin <peration0422@exntu.com>
2024-04-18 10:01:11 +0900

    style : Button Component 내 Disabled 스타일 별도 처리

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,11 +8,13 @@ import { cn } from "@/utils/cn";
 const buttonCSS = cva("rounded-sm px-3 py-2 shadow-md", {
   variants: {
     variant: {
-      primary: "cursor-pointer bg-blue-900 font-light text-white",
-      disabled: "bg-gray-200 font-normal text-gray-400",
-      outlined: "border border-gray-300 bg-white font-normal text-gray-800",
-      underlined: "font-normal underline underline-offset-4 shadow-none", // underline의 경우 custom으로 색상 조정하기
-      add: "flex flex-row items-center gap-1.5 border border-blue-900 bg-white font-normal text-blue-900",
+      primary:
+        "cursor-pointer bg-blue-900 font-light text-white disabled:bg-gray-200 disabled:font-normal disabled:text-gray-400",
+      outlined:
+        "border border-gray-300 bg-white font-normal text-gray-800 disabled:bg-gray-200 disabled:font-normal disabled:text-gray-400",
+      underlined:
+        "font-normal underline underline-offset-8 shadow-none disabled:font-normal disabled:text-gray-400", // underline의 경우 custom으로 색상 조정하기
+      add: "flex flex-row items-center gap-1.5 border border-blue-900 bg-white font-normal text-blue-900 disabled:border-gray-300 disabled:bg-gray-200 disabled:font-normal disabled:text-gray-400",
     },
   },
 });
@@ -27,7 +29,6 @@ const Button = ({ variant, type, className, text, ...props }: ButtonProps) => {
   return (
     <button
       type={type || "button"}
-      disabled={variant === "disabled"}
       className={cn(buttonCSS({ variant }), className)}
       {...props}
     >
