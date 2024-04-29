7da699d591fd7a1d2f4dfacf52e4b61e63598b06
Heeeera <happynet.choi@gmail.com>
2024-04-29 20:05:29 +0900

    feat : 예약 페이지 Banner 구현

---
+++ src/pages/Reservation/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+
+import Banner from "./components/Banner";
+
+const ReservationPage = () => {
+  return (
+    <Layout>
+      <section className="flex flex-col gap-8">
+        <Banner />
+      </section>
+    </Layout>
+  );
+};
+
+export default ReservationPage;
