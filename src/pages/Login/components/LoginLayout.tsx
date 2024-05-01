1c3dd05abd72d26cc7bd70050c2efec40921fba6
jeeseongmin <peration0422@exntu.com>
2024-05-01 16:35:06 +0900

    style : 로그인 페이지 > 메인 페이지 style 수정

--- src/pages/Login/components/LoginLayout.tsx
+++ src/pages/Login/components/LoginLayout.tsx
@@ -12,16 +12,16 @@ const LoginLayout = ({ children }: PropsWithChildren) => {
             className="h-full w-full rounded-l-md object-cover"
           />
           <div className="absolute right-0 top-0 h-full w-full rounded-l-md bg-blue-900/30 px-12 py-20 text-white">
-            <p className="text-md font-bold">Shalom!</p>
-            <p className="text-xl font-bold">평택 온누리교회 장소 예약</p>
+            <p className="text-[16px]">Shalom!</p>
+            <p className="text-[20px] font-bold">평택 온누리교회 장소 예약</p>
             <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center px-12 pb-0 text-center">
-              <div className="flex flex-col items-center justify-center">
-                <p className="text-sm">
+              <div className="flex flex-col items-center justify-center text-small">
+                <p>
                   “하나님의 나라를 전파하며 주 예수 그리스도에 관한 <br />
                   모든 것을 담대하게 거침없이 가르치더라”
                 </p>
                 <br />
-                <p className="font-bold">사도행전 28:31</p>
+                <p>사도행전 28:31</p>
               </div>
             </div>
           </div>
