c1f90604eb5a35dbc81dff4d4a844ca71c8428d4
jeeseongmin <peration0422@exntu.com>
2024-05-01 16:13:18 +0900

    style : Button 컴포넌트 스타일 변경

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,23 +6,26 @@ import { IoChatbubbleSharp } from "react-icons/io5";
 
 import { cn } from "@/utils/cn";
 
-const buttonCSS = cva("rounded-sm ", {
-  variants: {
-    variant: {
-      primary:
-        "cursor-pointer bg-blue-900 px-3 py-2 font-light text-white shadow-md disabled:bg-gray-light disabled:font-normal disabled:text-gray-400",
-      outlined:
-        "border border-gray-300 bg-white px-3 py-2 font-normal text-gray-800 shadow-md disabled:bg-gray-light disabled:font-normal disabled:text-gray-400",
-      underlined:
-        "px-3 py-2 font-normal underline underline-offset-8 shadow-md shadow-none disabled:font-normal disabled:text-gray-400", // underline의 경우 custom으로 색상 조정하기
-      add: "flex flex-row items-center gap-1.5 border border-blue-900 bg-white px-3 py-2 font-normal text-blue-900 shadow-md disabled:border-gray-300 disabled:bg-gray-light disabled:font-normal disabled:text-gray-400",
-      kakao:
-        "flex flex-row items-center gap-1.5 border border-yellow-300 bg-yellow-300 px-12 px-3 py-2 font-bold text-orange-900 shadow-md disabled:border-gray-300 disabled:bg-gray-light disabled:font-normal disabled:text-gray-400",
-      custom: "text-md",
-      icon: "text-md",
+const buttonCSS = cva(
+  "drop-shadow-base font-base	h-auto w-fit rounded-sm px-[11px] py-[8px] font-light",
+  {
+    variants: {
+      variant: {
+        primary:
+          "cursor-pointer bg-primary text-white disabled:border-gray-light disabled:bg-gray-light disabled:text-gray-dull",
+        outlined:
+          "border border-white bg-white text-gray-dark disabled:border-gray-light disabled:bg-gray-light disabled:text-gray-dull",
+        underlined:
+          "underline underline-offset-4 drop-shadow-none disabled:text-gray-400", // underline의 경우 custom으로 색상 조정하기
+        add: "flex flex-row items-center gap-1.5 border border-primary bg-white text-primary drop-shadow-none disabled:border-gray-light disabled:bg-gray-light disabled:text-gray-dull",
+        kakao:
+          "bg-kakaoBG text-kakaoFont flex flex-row items-center gap-1.5 px-16 py-3 font-semibold drop-shadow-none disabled:border-gray-light disabled:bg-gray-light disabled:text-gray-dull",
+        custom: "text-md",
+        icon: "text-md",
+      },
     },
   },
-});
+);
 type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonCSS>;
 
 const Button = ({
