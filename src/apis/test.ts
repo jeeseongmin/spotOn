f235e4441c64257c00388a18ba1731048871d7f1
jeeseongmin <peration0422@exntu.com>
2024-05-19 12:20:14 +0900

    feat : api 관련 helper util 추가

--- src/apis/test.ts
+++ src/apis/test.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
 
+import { METHOD, doFetch } from "@/lib/api/helper";
+
 axios.defaults.baseURL = "http://talkingclass.iptime.org:9000";
 axios.defaults.withCredentials = true;
 
 // Kakao login 후에 유저 정보에 token 값을 담아 서버에 호출
 export async function login(token: string) {
   try {
-    // email, password, provider
-    const response = await axios.post("/user-service/login", {
+    const res = await doFetch(METHOD.POST, "/user-service/login", {
       email: "1@gmail.com",
       password: "test1234!",
-      provider: "kakao",
+      provider: "email",
       token,
     });
 
-    console.log(response);
-    return response;
+    return res;
   } catch (e) {
     console.log(e);
   }
---
+++ src/lib/api/helper.ts
@@ -0,0 +1,41 @@
+import axios, { CancelToken } from "axios";
+
+export enum METHOD {
+  GET = "get",
+  POST = "post",
+  PUT = "put",
+  DELETE = "delete",
+}
+
+axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}`;
+axios.defaults.timeout = 5000;
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["x-api-key"] = "";
+axios.defaults.headers.common["Authorization"] = "";
+
+export const doFetch = async (
+  method: METHOD,
+  path: string,
+  params:
+    | Record<string, unknown>
+    | FormData
+    | Record<string, unknown>[]
+    | string
+    | null,
+  options?: {
+    headers?: Record<string, string>;
+    isLoading?: boolean;
+    cancelToken?: CancelToken;
+  },
+  timeout?: number,
+  noCache?: boolean,
+) => {
+  axios({
+    method,
+    url: path,
+    data: params,
+  }).then(res => {
+    console.log("res : ", method, res);
+  });
+};
