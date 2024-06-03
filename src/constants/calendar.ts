40beb4402a1cdf7b9c20da1f95788b7d02a556f8
jeeseongmin <peration0422@exntu.com>
2024-06-03 09:33:43 +0900

    feat : calendar util 함수 추가

--- src/constants/calendar.ts
+++ src/constants/calendar.ts
@@ -3,6 +3,14 @@ import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
 import { Dayjs } from "dayjs";
 
 export const daysOfTheWeek = ["일", "월", "화", "수", "목", "금", "토"];
+export const orderText = [
+  "",
+  "첫째 주",
+  "둘째 주",
+  "셋째 주",
+  "넷째 주",
+  "다섯째 주",
+];
 
 export interface CalendarProps {
   selectedDate?: Dayjs;
