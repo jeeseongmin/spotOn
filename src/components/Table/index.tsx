5da029d9e70fbe4f5b33afeb6a34219e3681d233
jeeseongmin <peration0422@exntu.com>
2024-06-18 17:47:27 +0900

    feat : Table Component 추가

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,85 @@
+import { ComponentPropsWithRef } from "react";
+
+import { VariantProps, cva } from "class-variance-authority";
+
+import Button from "@/components/Button";
+
+const tableContentCSS = cva("", {
+  variants: {
+    variant: {},
+  },
+});
+
+interface TableProps
+  extends VariantProps<typeof tableContentCSS>,
+    ComponentPropsWithRef<"table"> {
+  header: CellInfo[];
+  body: CellInfo[][];
+}
+
+const Table = ({ header, body, children, className, ...props }: TableProps) => {
+  const { variant } = props;
+
+  return (
+    <table id="table" className="w-full border-collapse">
+      <thead id="table-head" className="h-[44px] bg-gray-light text-base">
+        <tr>
+          {header.map(item => {
+            return (
+              <th className="w-auto border border-gray-middle font-light">
+                {item.name}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody id="table-body">
+        {body.map(items => {
+          return (
+            <tr className="h-[44px] text-center text-small">
+              {items.map(item => {
+                if (!item.name)
+                  return (
+                    <td className="border border-gray-middle">
+                      <Button
+                        variant="underlined"
+                        className="text-[12px] text-primary"
+                        onClick={item.method}
+                      >
+                        취소
+                      </Button>
+                    </td>
+                  );
+                else
+                  return (
+                    <td className="border border-gray-middle">{item.data}</td>
+                  );
+              })}
+              {/* <td className="border border-gray-middle">
+                2024 / 02 / 23 ~ 2024 / 12 / 31 매주 수요일
+              </td>
+              <td className="border border-gray-middle">21:00 ~ 23:00</td>
+              <td className="border border-gray-middle">
+                [3층] 301호 (P.W / POEM)
+              </td>
+              <td className="border border-gray-middle">
+                대청 주일 예배팀 연습
+              </td>
+              <td className="border border-gray-middle">승인대기</td>
+              <td className="border border-gray-middle">
+                <Button
+                  variant="underlined"
+                  className="text-[12px] text-primary"
+                >
+                  취소
+                </Button>
+              </td> */}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
