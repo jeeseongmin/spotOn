fac45a7886a61e633cc53da13e9073089a111eec
Heeeera <happynet.choi@gmail.com>
2024-07-01 16:01:01 +0900

    feat : react router v6 적용 가능하도록 storybook 설정 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,11 @@
 import type { Preview } from "@storybook/react";
 
+import { withRouter } from 'storybook-addon-remix-react-router';
+
 import "../src/index.css";
 
 const preview: Preview = {
+  decorators: [withRouter],
   parameters: {
     controls: {
       matchers: {
