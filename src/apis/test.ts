d227c0ebe6d91f523a2d31b59e2a93625cd1228e
jeeseongmin <peration0422@exntu.com>
2024-07-08 08:53:11 +0900

    feat : api 명세 변경

--- src/apis/test.ts
+++ src/apis/test.ts
@@ -2,18 +2,22 @@ import axios from "axios";
 
 import { METHOD, doFetch } from "@/lib/api/helper";
 
-axios.defaults.baseURL = "http://talkingclass.iptime.org:9000";
 axios.defaults.withCredentials = true;
 
 // Kakao login 후에 유저 정보에 코드 값을 담아 서버에 호출
 export async function login(code: string) {
   try {
-    const res = await doFetch(METHOD.POST, "/user-service/login", {
-      email: "1@gmail.com",
-      password: "test1234!",
-      provider: "kakao",
-      code,
-    });
+    const res = await doFetch(
+      METHOD.POST,
+      "http://localhost:8000/user-service/login",
+      {
+        email: "1@gmail.com",
+        password: "test1234!",
+        // provider: "email",
+        provider: "kakao",
+        token: code,
+      },
+    );
 
     return res;
   } catch (e) {
