ab80fc253eaf6597395f4d3404c5b06b3952d00f
peration0422 <peration0422@exntu.com>
2024-07-17 16:09:55 +0900

    chore : cors 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,24 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  server: {
+    proxy: {
+      // 카카오
+      "/kauth": {
+        target: "https://kauth.kakao.com/",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/kauth/, ""),
+        secure: false,
+        ws: true,
+      },
+      // 개발 서버
+      "/api": {
+        target: "http://localhost:8000/",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
