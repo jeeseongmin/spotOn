da2c9f7f18e71a44c07546b2645cc8169b115229
peration0422 <peration0422@exntu.com>
2024-08-17 20:28:33 +0900

    feat : 조직 관련 store 수정

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -4,10 +4,13 @@ import { persist } from "zustand/middleware";
 import { UserAction, UserState } from "@/types/store/userStore";
 
 const initialState: UserState = {
-  cmtCd: "", // 공동체명
-  cpsCd: "", // 캠퍼스명
-  garCd: "", // 다락방명
-  leafCd: "", // 순명
+  cpsCd: "", // 캠퍼스 코드
+  cmtCd: "", // 공동체 코드
+  garCd: "", // 다락방 코드
+  leafCd: "", // 순 코드
+  cmtNm: "", // 공동체명
+  garNm: "", // 다락방명
+  leafNm: "", // 순명
   roleId: "", // 역할명
   telNo: "", // 전화번호
   userName: "", // 사용자 이름
@@ -19,10 +22,13 @@ const useUserStore = create(
       ...initialState,
       saveUserInfo: (userInfo: UserState) =>
         set({
-          cmtCd: userInfo.cmtCd, // 공동체명
-          cpsCd: userInfo.cpsCd, // 캠퍼스명
-          garCd: userInfo.garCd, // 다락방명
-          leafCd: userInfo.leafCd, // 순명
+          cmtCd: userInfo.cmtCd, // 공동체 코드
+          cpsCd: userInfo.cpsCd, // 캠퍼스 코드
+          garCd: userInfo.garCd, // 다락방 코드
+          leafCd: userInfo.leafCd, // 순 코드
+          cmtNm: userInfo.cmtNm, // 공동체명
+          garNm: userInfo.garNm, // 다락방명
+          leafNm: userInfo.leafNm, //순명
           roleId: userInfo.roleId, // 역할명
           telNo: userInfo.telNo, // 전화번호
           userName: userInfo.userName, // 사용자 이름
@@ -33,6 +39,9 @@ const useUserStore = create(
           cpsCd: "",
           garCd: "",
           leafCd: "",
+          cmtNm: "",
+          garNm: "",
+          leafNm: "",
           roleId: "",
           telNo: "",
           userName: "",
