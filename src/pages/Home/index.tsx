1c9ec1c9545fa907b8701f100f15f9baf5fba603
Heeeera <happynet.choi@gmail.com>
2024-09-06 01:28:52 +0900

    fix : 홈화면 접근 시 캘린더 날짜 초기화

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+
 import Layout from "@/components/Layout";
 import Schedule from "@/components/Schedule";
 import RightPanel from "@/pages/Home/components/RightPanel";
+import useCalendarStore from "@/store/calendarStore";
 
 const Home = () => {
+  const resetCalendar = useCalendarStore(state => state.resetCalendar);
+
+  useEffect(() => {
+    resetCalendar();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout>
       <div className="flex flex-row justify-between gap-6 pb-32">
