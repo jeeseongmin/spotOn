2db294a7dd628a61474d07eea6437b0967098e7d
peration0422 <patrick.jee@kakaocorp.com>
2024-09-08 10:26:31 +0900

    feat : 사용자 페이지 내 쓰이는 type 정의

--- src/types/user.ts
+++ src/types/user.ts
@@ -5,6 +5,27 @@ export interface User {
   community: string;
 }
 
+export type UserResponse = {
+  cmtCd: string;
+  cmtNm: string;
+  cpsCd: string;
+  createdDate: string;
+  email: string;
+  garCd: string;
+  garNm: string;
+  lastLoginDate: string;
+  laefCd: string;
+  leafNm: string;
+  loginFailCount: number;
+  roleId: string;
+  roleName: string;
+  telNo: string;
+  userId: string;
+  userName: string;
+  userStateCode: string;
+  userStateCodeName: string;
+};
+
 /**
  * 사용자 상태 코드
  *
@@ -14,7 +35,20 @@ export interface User {
  * 08: 탈퇴
  * 09: 삭제
  */
+export type UserStateCode = "00" | "01" | "07" | "08" | "09";
 export interface UserStateRequest {
   userId: string;
-  userStateCode: "00" | "01" | "07" | "08" | "09";
+  userStateCode: string;
+}
+
+export interface UserByState
+  extends Omit<UserResponse, "bldCd" | "crtId" | "crDt" | "updId" | "updDt"> {
+  userName: string;
+  telNo: string;
+  cmtCd: string;
+  cmtNm: string;
+  garCd: string;
+  garNm: string;
+  leafCd: string;
+  leafNm: string;
 }
