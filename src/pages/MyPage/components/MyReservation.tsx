5e938abca8646c7c7df1f35d3fe28e82f992a45d
peration0422 <patrick.jee@kakaocorp.com>
2024-09-10 16:19:39 +0900

    feat : 나의 예약 페이지 내 api 매핑

--- src/pages/MyPage/components/MyReservation.tsx
+++ src/pages/MyPage/components/MyReservation.tsx
@@ -1,35 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 
+import { getReservationByUsers, putReservationState } from "@/apis/reservation";
 import Button from "@/components/Button";
 import AlertModal from "@/components/Modal/AlertModal";
 import ConfirmModal from "@/components/Modal/ConfirmModal";
 import Table from "@/components/Table";
-import { titleList } from "@/dummy/table";
 import useModal from "@/hooks/useModal";
 import MyPageWrapper from "@/pages/MyPage/components/MyPageLayout";
-import { getTableBody, getTableHeader } from "@/utils/table";
+import useLoginStore from "@/store/loginStore";
+import { ReservationByState, ReservationStateCode } from "@/types/reservation";
+import { CellInfo } from "@/types/table";
+import {
+  getMyReservationBodyData,
+  getTableBody,
+  getTableHeader,
+} from "@/utils/table";
+
+const reservationTableHeader = ["날짜", "시간", "장소", "사용목적", "상태", ""];
+const header = getTableHeader(reservationTableHeader);
 
 const MyReservation = () => {
-  const [headerList] = useState(getTableHeader(titleList));
-  const [bodyList] = useState(
-    getTableBody(titleList, [
-      [
-        { name: "날짜", data: `2024 / 02 / 23 ~ 2024 / 12 / 31 \n매주 수요일` },
-        { name: "시간", data: "21:00 ~ 23:00" },
-        { name: "장소", data: "[3층] 301호 (P.W / POEM)" },
-        { name: "사용목적", data: "대청 주일 예배팀 연습" },
-        { name: "상태", data: "승인 대기" },
-        { name: "", data: "취소", method: () => confirmModal.onOpen() },
-      ],
-    ]),
-  );
+  const { tokenId } = useLoginStore();
+  const [reservationBody, setReservationBody] = useState<CellInfo[][]>([]);
+  const [selectedReservation, setSelectedReservation] =
+    useState<ReservationByState | null>(null);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const [reservations, setReservations] = useState([]);
 
   const confirmModal = useModal();
   const alertModal = useModal();
 
+  const changeReservationState = async (
+    rsvtId: number,
+    sttCd: ReservationStateCode,
+  ) => {
+    await putReservationState({ rsvtId, sttCd });
+    getMyReservation();
+  };
+
+  useEffect(() => {
+    const reservationTableBody = reservations.map(reservation => {
+      const { sttCd } = reservation;
+
+      const cancelReservation = () => {
+        setSelectedReservation(reservation);
+        confirmModal.onOpen();
+      };
+
+      return getMyReservationBodyData(reservation, sttCd, cancelReservation);
+    });
+
+    setReservationBody(
+      getTableBody(reservationTableHeader, reservationTableBody),
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reservations]);
+
+  useEffect(() => {
+    getMyReservation();
+  }, []);
+
+  const getMyReservation = async () => {
+    const { content } = await getReservationByUsers(0, 10, tokenId);
+    setReservations(content);
+  };
+
   const onConfirm = () => {
+    if (!selectedReservation) {
+      setAlertMessage("요청을 처리할 수 없습니다. 다시 시도해 주세요.");
+    } else {
+      changeReservationState(selectedReservation?.rsvtId, "cancel");
+      setAlertMessage("예약이 취소되었습니다.");
+    }
+
     alertModal.onOpen();
     confirmModal.onClose();
   };
@@ -37,7 +83,7 @@ const MyReservation = () => {
   return (
     <MyPageWrapper>
       <div className="flex h-96 w-full flex-col justify-between p-4">
-        <Table header={headerList} body={bodyList} />
+        <Table header={header} body={reservationBody} />
         <div className="flex items-center justify-center gap-4">
           <Button variant="icon" className="text-gray-dark">
             <MdArrowBackIosNew size={14} />
@@ -58,9 +104,7 @@ const MyReservation = () => {
         </ConfirmModal>
       )}
       {alertModal.isOpen && (
-        <AlertModal onClose={alertModal.onClose}>
-          예약이 취소되었습니다.
-        </AlertModal>
+        <AlertModal onClose={alertModal.onClose}>{alertMessage}</AlertModal>
       )}
     </MyPageWrapper>
   );
